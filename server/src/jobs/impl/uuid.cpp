61491a03fb1b5f805bab0c2cc7b8be49c1cb46ea
Mauro Guerrera <mauroguerrera@eaton.com>
2020-11-23 16:59:02 +0000

    Add UUID generation during discovery
    Signed-off-by: Mauro Guerrera <mauroguerrera@eaton.com>

---
+++ server/src/jobs/impl/uuid.cpp
@@ -0,0 +1,60 @@
+/*  ====================================================================================================================
+    Copyright (C) 2020 Eaton
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    ====================================================================================================================
+*/
+
+#include "uuid.h"
+#include <fty_log.h>
+#include <openssl/sha.h>
+#include <uuid/uuid.h>
+
+namespace fty::impl {
+
+std::string generateUUID(const std::string& manufacturer, const std::string& model, const std::string& serial)
+{
+    static std::string ns = "\x93\x3d\x6c\x80\xde\xa9\x8c\x6b\xd1\x11\x8b\x3b\x46\xa1\x81\xf1";
+    std::string        uuid;
+
+    if (!manufacturer.empty() && !model.empty() && !serial.empty()) {
+        log_debug("generate full UUID");
+
+        std::string src = ns + manufacturer + model + serial;
+        // hash must be zeroed first
+        unsigned char* hash = static_cast<unsigned char*>(calloc(SHA_DIGEST_LENGTH, sizeof(unsigned char)));
+
+        SHA1(reinterpret_cast<const unsigned char*>(src.c_str()), src.length(), hash);
+
+        hash[6] &= 0x0F;
+        hash[6] |= 0x50;
+        hash[8] &= 0x3F;
+        hash[8] |= 0x80;
+
+        char uuid_char[37];
+        uuid_unparse_lower(hash, uuid_char);
+
+        uuid = uuid_char;
+    } else {
+        uuid_t u;
+        uuid_generate_random(u);
+
+        char uuid_char[37];
+        uuid_unparse_lower(u, uuid_char);
+
+        uuid = uuid_char;
+    }
+
+    return uuid;
+}
+
+}
