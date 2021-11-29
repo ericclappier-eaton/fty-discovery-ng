8af9f3b1775ff54dfe946077175be1edce44af69
Hlib Yarovyi <HlibYarovyi@eaton.com>
2021-11-29 09:08:40 +0100

    Config read/create. Discovery config fix types
    Signed-off-by: Hlib Yarovyi <HlibYarovyi@eaton.com>

--- common/discovery-config.cpp
+++ common/discovery-config.cpp
@@ -1,116 +1,19 @@
 #include "discovery-config.h"
 #include <fstream>
 #include <map>
-#define WITH_ZCONFIG 1
 #include <pack/serialization.h>
 
 namespace fty::disco {
-/*
-ConfigDiscovery& ConfigDiscovery::operator+=(const zproject::Argument& arg)
-{
-    auto var = arg.first;
-    if(var == zproject::Type){
-        std::stringstream ss(arg.second);
-        auto tt = ConfigDiscovery::Discovery::Type::Unknown;
-        ss >> tt;
-        discovery.type = tt;
-    } else if(var == zproject::Scans) {
-        discovery.scans.append(arg.second);
-    }
-    else if(var == zproject::Ips) {
-        discovery.ips.append(arg.second);
-    }
-    else if(var == zproject::Documents) {
-        discovery.documents.append(arg.second);
-    }
-    else if(var == zproject::Protocols) {
-        discovery.protocols.append(arg.second);
-    }
-
-    else if(var == zproject::DefaultValStatus) {
-        aux.status = arg.second;
-    }
-    else if(var == zproject::DefaultValPriority) {
-        aux.priority = fty::convert<pack::Int32>(arg.second);
-    }
-    else if(var == zproject::DefaultValParent) {
-        aux.parent = arg.second;
-    }
-
-    else if(var == zproject::DefaultValLinkSrc) {
-        links.append().src = arg.second;
-    }
-
-    else if(var == zproject::ScansDisabled) {
-        disabled.scans
-    }
-    else if(var == zproject::IpsDisabled) {}
-
-    else if(var == zproject::DumpPool) {}
-    else if(var == zproject::ScanPool) {}
-    else if(var == zproject::ScanTimeout) {}
-    else if(var == zproject::DumpLooptime) {}
-
-    std::map<zproject::Variable, pack::Attribute&> variables =
-        // clang-format off
-    {
-        { zproject::Type, this->discovery.type},
-        { zproject::Scans, this->discovery.scans },
-        { zproject::Ips, this->discovery.ips },
-        { zproject::Documents, this->discovery.documents },
-        { zproject::Protocols, this->discovery.protocols },
-
-        { zproject::DefaultValStatus, this->aux.status },
-        { zproject::DefaultValPriority, this->aux.priority },
-        { zproject::DefaultValParent, this->aux.parent },
-
-        { zproject::DefaultValLinkSrc, this->links[0].src },
-        { zproject::ScansDisabled, this->disabled.scans },
-        { zproject::IpsDisabled, this->disabled.ips },
-
-        { zproject::DumpPool, this->parameters.maxDumpPoolNumber },
-        { zproject::ScanPool, this->parameters.maxScanPoolNumber },
-        { zproject::ScanTimeout, this->parameters.nutScannerTimeOut },
-        { zproject::DumpLooptime, this->parameters.dumpDataLoopTime },
-    };
-    // clang-format on
-
-    parameters.dumpDataLoopTime.typeName();
-    auto &var = variables[arg.first];
-    // var.ThisType
-    // var = fty::convert<var.>(arg.second);
-
-    return *this;
-}
-
- */
 
 fty::Expected<void> ConfigDiscovery::save(const std::string& path)
 {
     return pack::yaml::serializeFile(path, *this, pack::Option::WithDefaults);
 }
 
-namespace zproject {
-    fty::Expected<void> saveToFile(const ConfigDiscovery& config, const std::string& path)
-    {
-        std::ofstream file;
-        file.exceptions(std::ofstream::badbit | std::ofstream::failbit);
-        try {
-            file.open(path);
-            file << *pack::zconfig::serialize(config, pack::Option::ValueAsString | pack::Option::WithDefaults)
-                 << std::endl;
-            file.close();
-        } catch (const std::ofstream::failure& e) {
-            return fty::unexpected("save file failure: {}", e.what());
-        } catch (const std::exception& e) {
-            return fty::unexpected("save file exeption: {}", e.what());
-        } catch (...) {
-            return fty::unexpected("save file unknown exeption");
-        }
-
-        return {};
-    }
-} // namespace zproject
+fty::Expected<void> ConfigDiscovery::load(const std::string& path)
+{
+    return pack::yaml::deserializeFile(path, *this);
+}
 
 std::ostream& operator<<(std::ostream& ss, ConfigDiscovery::Discovery::Type value)
 {
---
+++ rest/src/config-create.cpp
@@ -0,0 +1,82 @@
+#include "config-create.h"
+#include "commands.h"
+#include "discovery-config.h"
+#include <fty/rest/component.h>
+
+namespace fty::disco {
+
+unsigned ConfigCreate::run()
+{
+    rest::User user(m_request);
+    if (auto ret = checkPermissions(user.profile(), m_permissions); !ret) {
+        throw rest::Error(ret.error());
+    }
+
+    commands::config::In in;
+    if (auto ret = pack::json::deserialize(m_request.body(), in); !ret) {
+        throw rest::errors::Internal(ret.error());
+    }
+
+    ConfigDiscovery cd;
+    if (auto ret = cd.load(); !ret) {
+        throw rest::errors::Internal(ret.error());
+    }
+
+    if (in.type.hasValue()) {
+        std::stringstream                ss(in.type.value());
+        ConfigDiscovery::Discovery::Type tmpType;
+        ss >> tmpType;
+        cd.discovery.type = tmpType;
+    }
+    if (in.scans.hasValue()) {
+        cd.discovery.scans = in.scans;
+    }
+    if (in.ips.hasValue()) {
+        cd.discovery.ips = in.ips;
+    }
+    if (in.docs.hasValue()) {
+        cd.discovery.documents = in.docs;
+    }
+    if (in.status.hasValue()) {
+        cd.aux.status = in.status;
+    }
+    if (in.priority.hasValue()) {
+        cd.aux.priority = in.priority;
+    }
+    if (in.parent.hasValue()) {
+        cd.aux.parent = in.parent;
+    }
+    if (in.linkSrc.hasValue()) {
+        cd.link.src = in.linkSrc;
+    }
+    if (in.scansDisabled.hasValue()) {
+        cd.disabled.scans = in.scansDisabled;
+    }
+    if (in.ipsDisabled.hasValue()) {
+        cd.disabled.ips = in.ipsDisabled;
+    }
+    if (in.protocols.hasValue()) {
+        cd.discovery.protocols = in.protocols;
+    }
+    if (in.dumpPool.hasValue()) {
+        cd.parameters.maxDumpPoolNumber = in.dumpPool;
+    }
+    if (in.scanPool.hasValue()) {
+        cd.parameters.maxScanPoolNumber = in.scanPool;
+    }
+    if (in.scanTimeout.hasValue()) {
+        cd.parameters.nutScannerTimeOut = in.scanTimeout;
+    }
+    if (in.dumpLooptime.hasValue()) {
+        cd.parameters.dumpDataLoopTime = in.dumpLooptime;
+    }
+
+    if (auto ret = cd.save(); !ret) {
+        throw rest::errors::Internal(ret.error());
+    }
+    return HTTP_OK;
+}
+
+} // namespace fty::disco
+
+registerHandler(fty::disco::ConfigCreate)
