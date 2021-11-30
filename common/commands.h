3fda541d8da10cd93c2ae74cfe7277708adc9ce1
Clappier, Eric <EricClappier@Eaton.com>
2021-11-30 07:40:14 +0100

    Take into account PR remarks (suite)

--- common/commands.h
+++ common/commands.h
@@ -319,10 +319,6 @@ namespace commands::discoveryauto {
 
 } // namespace commands::discoveryauto
 
-namespace commands {
-    using InVar = pack::Variant<commands::discoveryauto::In, commands::assets::In>;
-}
-
 // =====================================================================================================================
 
 } // namespace fty
--- server/src/jobs/assets.h
+++ server/src/jobs/assets.h
@@ -24,7 +24,7 @@ namespace fty::job {
 
 struct DeviceInfo {
     int indexDevice;
-    std::string& deviceSerial;
+    std::string deviceSerial;
     std::string type;
 };
 
