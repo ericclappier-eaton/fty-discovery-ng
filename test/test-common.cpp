ae31e64fd28f0a9032feb54d709dbd4c84828576
Clappier, Eric <EricClappier@Eaton.com>
2022-01-03 18:36:48 +0100

    Fix test issue

---
+++ test/test-common.cpp
@@ -0,0 +1,73 @@
+#include "test-common.h"
+
+Test::Test() : m_dis("conf/discovery.conf")
+{
+}
+
+Test::~Test()
+{
+    zactor_destroy(&m_broker);
+}
+
+fty::disco::Message Test::createMessage(const char* subject)
+{
+    fty::disco::Message msg;
+    msg.meta.to      = fty::disco::Config::instance().actorName;
+    msg.meta.subject = subject;
+    msg.meta.from    = "unit-test";
+    return msg;
+}
+
+fty::Expected<fty::disco::Message> Test::send(const fty::disco::Message& msg)
+{
+    return Test::instance().m_bus.send(fty::disco::Channel, msg);
+}
+
+fty::Expected<void> Test::init()
+{
+    if (auto ret = Test::instance().m_dis.loadConfig(); !ret) {
+        return fty::unexpected("Cannot load config");
+    }
+
+    fty::disco::impl::Snmp::instance().init(fty::disco::Config::instance().mibDatabase);
+    ManageFtyLog::setInstanceFtylog(fty::disco::Config::instance().actorName, fty::disco::Config::instance().logConfig);
+
+    // Create the broker
+    static const char* endpoint_disco = "inproc://fty-discovery-ng-test";
+    Test::instance().m_broker = zactor_new(mlm_server, const_cast<char*>("Malamute"));
+    zstr_sendx(Test::instance().m_broker, "BIND", endpoint_disco, NULL);
+    zstr_send(Test::instance().m_broker, "VERBOSE");
+
+    if (auto res = Test::instance().m_dis.init(); !res) {
+        return fty::unexpected(res.error());
+    }
+
+    if (auto res = Test::instance().m_bus.init("unit-test", endpoint_disco); !res) {
+        return fty::unexpected(res.error());
+    }
+
+    Test::instance().m_th = std::thread([&]() {
+        Test::instance().m_dis.run();
+    });
+    return {};
+}
+
+fty::disco::Discovery& Test::getDisco() {
+    return Test::instance().m_dis;
+}
+
+fty::disco::MessageBus& Test::getBus() {
+    return Test::instance().m_bus;
+}
+
+void Test::shutdown()
+{
+    Test::instance().m_dis.shutdown();
+    Test::instance().m_th.join();
+}
+
+Test& Test::instance()
+{
+    static Test test;
+    return test;
+}
--- test/test-common.h
+++ test/test-common.h
@@ -12,97 +12,29 @@
 #include <mlm_library.h>
 #include <mlm_server.h>
 
-// TBD
-#include <czmq.h>
-#include <fstream>
-#include <fty_common_mlm.h>
-
-class Test;
-inline Test* inst;
-
 class Test
 {
 public:
-    ~Test()
-    {
-        //zactor_destroy(&m_broker);
-    }
+    Test();
+    ~Test();
 
-    static fty::disco::Message createMessage(const char* subject)
-    {
-        fty::disco::Message msg;
-        msg.meta.to      = fty::disco::Config::instance().actorName;
-        msg.meta.subject = subject;
-        msg.meta.from    = "unit-test";
-        return msg;
-    }
+    static fty::disco::Message createMessage(const char* subject);
 
-    static fty::Expected<fty::disco::Message> send(const fty::disco::Message& msg)
-    {
-        return inst->m_bus.send(fty::disco::Channel, msg);
-    }
+    static fty::Expected<fty::disco::Message> send(const fty::disco::Message& msg);
 
-    /*static void subscribe(const std::string& queue, std::function<void(const messagebus::Message&)>&& fun)
-    {
-        return inst->m_bus.subscribe<>(fty::disco::Channel, msg);
-    }*/
+    static fty::Expected<void> init();
 
-    static fty::Expected<void> init()
-    {
-        inst = new Test;
-        if (auto ret = inst->m_dis.loadConfig(); !ret) {
-            return fty::unexpected("Cannot load config");
-        }
+    static fty::disco::Discovery& getDisco();
 
-        fty::disco::impl::Snmp::instance().init(fty::disco::Config::instance().mibDatabase);
-        ManageFtyLog::setInstanceFtylog(fty::disco::Config::instance().actorName, fty::disco::Config::instance().logConfig);
+    static fty::disco::MessageBus& getBus();
 
-// TBD
-#if 0
-        // create the broker
-        static const char* endpoint_disco = "inproc://fty-discovery-ng-test";
-        zactor_t *broker = inst->m_broker;//.get();
-        zstr_sendx(inst->m_broker, "BIND", endpoint_disco, NULL);
-        zstr_send(inst->m_broker, "VERBOSE");
-#endif
+    static void shutdown();
 
-        if (auto res = inst->m_dis.init(); !res) {
-            return fty::unexpected(res.error());
-        }
-
-        if (auto res = inst->m_bus.init("unit-test"); !res) {
-            return fty::unexpected(res.error());
-        }
-
-        inst->m_th = std::thread([&]() {
-            inst->m_dis.run();
-        });
-        return {};
-    }
-
-    fty::disco::Discovery& getDisco() {
-        return m_dis;
-    }
-
-    static void shutdown()
-    {
-        inst->m_dis.shutdown();
-        inst->m_th.join();
-        delete inst;
-    }
-
-private:
-    Test()
-        : m_dis("conf/discovery.conf")
-          //m_broker(zactor_new(mlm_server, const_cast<char*>("Malamute")))
-    {
-    }
+    static Test& instance();
 
 private:
     fty::disco::Discovery     m_dis;
+    zactor_t*                 m_broker = nullptr;
     std::thread               m_th;
     fty::disco::MessageBus    m_bus;
-    // TBD
-    //std::unique_ptr<zactor_t> m_broker;
-    //zactor_t*                 m_broker = nullptr;
 };
