77633a09339ee205fbc4b71e2dac7776382332c6
Clappier, Eric <EricClappier@Eaton.com>
2022-02-02 15:51:03 +0100

    Fefactor core side according rest modification

--- server/src/jobs/scan-status.cpp
+++ server/src/jobs/scan-status.cpp
@@ -8,20 +8,7 @@ void ScanStatus::run(commands::scan::status::Out& out)
     using namespace commands::scan::status;
 
     auto status = m_autoDiscovery->getStatus();
-    switch (status.state) {
-        case AutoDiscovery::State::CancelledByUser:
-            out.status = Out::Status::CancelledByUser;
-            break;
-        case AutoDiscovery::State::Terminated:
-            out.status = Out::Status::Terminated;
-            break;
-        case AutoDiscovery::State::InProgress:
-            out.status = Out::Status::InProgress;
-            break;
-        case AutoDiscovery::State::Unknown:
-            out.status = Out::Status::Unknown;
-            break;
-    }
+    out.status = status.state;
     out.progress = std::to_string(status.progress) + "%";
     out.discovered = status.discovered;
     out.ups = status.ups;
