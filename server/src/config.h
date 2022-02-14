fcd481be3643d8062d4015e204ba368e05e37ca3
Clappier, Eric <EricClappier@Eaton.com>
2022-02-14 15:50:00 +0100

    Stub security wallet

--- server/src/config.h
+++ server/src/config.h
@@ -32,12 +32,11 @@ public:
     pack::String mibDatabase = FIELD("mib-database", "mibs");
     pack::Bool   tryAll      = FIELD("try-all", false);
     pack::UInt32 pollScanMax = FIELD("poll-scan-max", 50);
-    pack::String secwSocket  = FIELD("secw-socket", "/run/fty-security-wallet/secw.socket");
     pack::String endpoint    = FIELD("endpoint", "ipc://@/malamute");
 
 public:
     using pack::Node::Node;
-    META(Config, actorName, logConfig, mibDatabase, tryAll, pollScanMax, secwSocket, endpoint);
+    META(Config, actorName, logConfig, mibDatabase, tryAll, pollScanMax, endpoint);
 
 public:
     static Config& instance();
