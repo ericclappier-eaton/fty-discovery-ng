24d76473212d67ceba57f27bfecab29d68b33cb8
Clappier, Eric <EricClappier@eaton.com>
2023-01-24 14:00:53 +0100

    Fix unicity of client name with malamute

--- rest/src/discovery-rest.h
+++ rest/src/discovery-rest.h
@@ -3,6 +3,8 @@
 
 namespace fty::disco {
 
+static const char* AGENT_DISCOVERY_NAME = "fty-discovery-ng";
+
 inline fty::disco::Message message(const std::string& subject, const std::string& clientName)
 {
     fty::disco::Message msg;
@@ -10,7 +12,7 @@ inline fty::disco::Message message(const std::string& subject, const std::string
     msg.meta.replyTo = clientName;
     msg.meta.from    = clientName;
     msg.meta.subject = subject;
-    msg.meta.to      = "fty-discovery-ng";
+    msg.meta.to      = AGENT_DISCOVERY_NAME;
 
     return msg;
 }
